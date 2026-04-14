44488e6360eeae462d022e43179d69e9e12dad57
0obito <elmsafer.abdellah00@gmail.com>
2026-04-14 17:42:01 +0100

    simple Server and Client to establish connection

---
+++ CsocketServer.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main() {
+    int         sock_fd;
+    struct sockaddr_in sock_add;
+
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+                // param 1: specifies what kind of addresses we're dealing with. AF_LOCAL for local IPC (on the same computer), AF_INET for IPC over the INTERNTET (IPv4).
+                // param 2: specifies what type of communication will be used. SOCK_STREAM for a reliable connection-oriented byte stream, SOCK_DGRAM for connectionless, unreliable messages.
+                // param 3: specifies a particular protocol to be used with the socket, 0 is for default protocol to the selected domain and type.
+    if (sock_fd < 0)
+        perror("socket()");
+    sock_add.sin_family = AF_INET;
+    sock_add.sin_port = htons(6969);
+    sock_add.sin_addr = htons(INADDR_ANY);
+    if (bind(sock_fd, (const struct sockaddr *)&sock_add, sizeof(sock_add)) < 0)
+        perror("bind()");
+    if (listen(sock_fd, 1) < 0)
+        perror("listen()");
+    if (accept(, ) < 0)
+        perror("accept()");
+    return (0);
+}
