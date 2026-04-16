e6e2c95226477ea08170dafab5c39777fd0c1673
0obito <elmsafer.abdellah00@gmail.com>
2026-04-16 17:50:09 +0100

    [Update] client

--- CsocketClient.c
+++ CsocketClient.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int	main() {
+    int clientFD;
+    struct sockaddr_in  servAddress;
+    int                 connectRet;
+
+    clientFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientFD < 0)
+        perror("socket()");
+    servAddress.sin_family = AF_INET;
+    servAddress.sin_addr.s_addr = (1 << 24) + 127;
+    servAddress.sin_port = htons(6969);
+    connectRet = connect(clientFD, (const struct sockaddr *)&servAddress, sizeof(servAddress));
+    if (connect != 0)
+        perror("connect()");
+    // Next on: Talk with server!
+    write();
+    return (0);
 }
