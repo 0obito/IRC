f359c91e6bf4b0ea6ef4e28d7770bcb0636c7b69
0obito <elmsafer.abdellah00@gmail.com>
2026-04-24 21:24:12 +0100

    [UPDATE] I tried more 'communication' between the server and the client, by having them go back and forth.

--- CsocketClient.c
+++ CsocketClient.c
@@ -9,6 +9,7 @@ int	main() {
     int clientFD;
     struct sockaddr_in  servAddress;
     int                 connectRet;
+    char                server_request[2000];
 
     clientFD = socket(AF_INET, SOCK_STREAM, 0);
     if (clientFD < 0)
@@ -19,9 +20,28 @@ int	main() {
     connectRet = connect(clientFD, (const struct sockaddr *)&servAddress, sizeof(servAddress));
     if (connectRet != 0)
         perror("connect()");
-    // Next on: Talk with server!
-    char    *msg = "This is the client, hey there!\n";
-    write(clientFD, msg, strlen(msg));
+
+    char    *cli_msg_one = "[CLIENT] I'm alive!\n";
+
+    char    *cli_msg_two = "Hey there Mr Server!\n";
+    char    *sent = "[CLIENT] message sent.\n";
+    char    *received = "[CLIENT] message received.\n";
+    char    *extra = "[CLIENT] from [SERVER]: ";
+    char    *done = "[CLIENT] ---- Client is out! ----\n";
+
+    memset(server_request, 0, sizeof(server_request));
+
+    write(1, cli_msg_one, strlen(cli_msg_one));
+
+    write(clientFD, cli_msg_two, strlen(cli_msg_two));
+    write(1, sent, strlen(sent));
+
+    ssize_t readdd= read(clientFD, server_request, sizeof(server_request));
+    write(1, received, strlen(received));
+    write(1, extra, strlen(extra));
+    write(1, server_request, readdd/*strlen(server_request*/);
+
     close(clientFD);
+    write(1, done, strlen(done));
     return (0);
 }
--- CsocketServer.c
+++ CsocketServer.c
@@ -11,7 +11,7 @@ int main() {
     struct sockaddr_in  client_add;
     socklen_t           client_add_size;
     int                 client_fd;
-    char                request[2000];
+    char                client_request[2000];
 
     sock_fd = socket(AF_INET, SOCK_STREAM, 0);
                 // param 1: specifies what kind of addresses we're dealing with. AF_LOCAL for local IPC (on the same computer), AF_INET for IPC over the INTERNTET (IPv4).
@@ -33,10 +33,29 @@ int main() {
     client_fd = accept(sock_fd, (struct sockaddr *)&client_add, &client_add_size);
     if (client_fd < 0)
         perror("accept()");
-    memset(request, 0, sizeof(request));
-    read(client_fd, request, sizeof(request));
-    write(1, request, sizeof(request));
+
+    char *srv_msg_one = "[SERVER] I'm alive!\n";
+
+    char *srv_msg_two = "I got you Mr Client! How can I help you today?\n";
+    char    *sent = "[SERVER] message sent.\n";
+    char    *received = "[SERVER] message received.\n";
+    char    *extra = "[SERVER] from [CLIENT]: ";
+    char    *done = "[SERVER] ---- Server is out! ----\n";
+
+    memset(client_request, 0, sizeof(client_request));
+
+    write(1, srv_msg_one, strlen(srv_msg_one));
+
+    int readdd = read(client_fd, client_request, sizeof(client_request));
+    write(1, received, strlen(received));
+    write(1, extra, strlen(extra));
+    write(1, client_request, readdd/*sizeof(client_request)*/);
+
+    write(client_fd, srv_msg_two, strlen(srv_msg_two));
+    write(1, sent, strlen(sent));
+
     close(client_fd);
     close(sock_fd);
+    write(1, done, strlen(done));
     return (0);
 }
