1e94a6247b3291eeb184407fb37cd15ffd2de77b
youness elouam <elouamy@gmail.com>
2026-05-03 17:21:34 +0100

    server socket

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -1,11 +1,28 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
 
-// Created the server class backbone, Youness will have to fill this and complete it!
-
-#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <unistd.h>
 
 class Server {
-    Server();
+private:
+    int                     serversocket;
+    int                     port;
+    struct sockaddr_in      address;
+    std::string             password;
+    // std::vector<pollfd>     fds;
+
+public:
+    Server(int port_in, std::string pwd);
     Server(const Server& other);
     const Server& operator=(const Server& other);
     ~Server();
+
+    void    initserver();
+    void    run();
+    void    acceptNewClient();
 };
+
+#endif
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -1,17 +1,40 @@
-
-// Created the server class backbone, Youness will have to fill this and complete it!
-
-#include <string>
 #include "includes/Server.hpp"
 
-Server::Server() {
+void Server::initserver()
+{
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = INADDR_ANY;
+}
+
+void    Server::run()
+{
+    serversocket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    int opt = 1;
+    setsockopt(serversocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    bind(serversocket, (sockaddr*)&address, sizeof(address));
+    listen(serversocket, SOMAXCONN);
+}
+
+Server::Server(int port_in, std::string pwd) {
+    port = port_in;
+    password = pwd;
+    initserver();
+    run();
 }
 
 Server::Server(const Server& other) {
+    *this = other;
 }
 
 const Server& Server::operator=(const Server& other) {
+    if (this == &other)
+        return(*this);
+    port = other.port;
+    password = other.password;
+    return(*this);
 }
 
 Server::~Server() {
+    close(serversocket);
 }
