c9c4442ec044dc3b3f7f0ae88f4b5dda74ea0702
0obito <elmsafer.abdellah00@gmail.com>
2026-05-05 18:21:54 +0100

    [SAVE] Saving my work from today, huge changes lol

--- srcs/DispatcherUtils.cpp
+++ srcs/DispatcherUtils.cpp
@@ -6,7 +6,7 @@ void handleCAP(Server& server, Client& client, Command& parsedMsg) {
 }
 
 void handlePASS(Server& server, Client& client, Command& parsedMsg) {
-    if (client.isRegistered) {
+    if (client.isRegistered == true) {
         std::cout << "ERR_ALREADYREGISTERED (462)" << std::endl;
         return ;
     }
@@ -24,7 +24,44 @@ void handlePASS(Server& server, Client& client, Command& parsedMsg) {
     }
 }
 
+bool nickIsValid(std::string &nickName) {
+    if (nickname.empty())
+        return false;
+    if (nickname.find(' ') != std::string::npos || nickname.find(',') != std::string::npos || nickname.find('*') != std::string::npos || nickname.find('.') != std::string::npos
+        || nickname.find('?') != std::string::npos || nickname.find('!') != std::string::npos || nickname.find('@') != std::string::npos)
+        return false;
+    if (nickname[0] == '$' || nickname[0] == ':' || nickname[0] == '#' || nickname[0] == '&'
+        || nickname[0] == '~' || nickname[0] == '%' || nickname[0] == '+')
+        return false;
+    return true;
+}
+
 void handleNICK(Server& server, Client& client, Command& parsedMsg) {
+    if (parsedMsg.params.empty() == true) {
+        // NR 431: Returned when a nickname parameter is expected for a command but isn’t given.
+        std::cout << "ERR_NONICKNAMEGIVEN (431)" << std::endl;
+        return ;
+    }
+    std::string nickName = parsedMsg.params[0];
+    if (client.getNickOk == true && client.getNick == nickName) {
+        // the requested nickname is the same as the existing older nickname, nothing to do.
+        return ;
+    }
+    if (server.isNicknameTaken(parsedMsg.params[0]) == true) {
+        // NR 433: Returned when a NICK command cannot be successfully completed as the desired nickname is already in use on the network.
+        std::cout << "ERR_NICKNAMEINUSE (433)" << std::endl;
+        return ;
+    }
+    if (nickIsValid(nickName) == true) {
+        // the requested nickname is fine, set it.
+        client.setNick(nickName);
+        return ;
+    }
+    else {
+        // NR 432: Returned when a NICK command cannot be successfully completed as the desired nickname contains characters that are disallowed by the server.
+        std::cout << "ERR_ERRONEUSNICKNAME (432)" << std::endl;
+        return ;
+    }
 }
 
 void handleUSER(Server& server, Client& client, Command& parsedMsg) {
