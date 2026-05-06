65326c21b033ac79596eb161ec1dbf43fa4b1f6a
youness elouam <elouamy@gmail.com>
2026-05-06 17:46:42 +0100

    creat epoll instaance and client map

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -2,16 +2,21 @@
 #define SERVER_HPP
 
 #include <sys/socket.h>
+#include <sys/epoll.h>
 #include <netinet/in.h>
 #include <iostream>
 #include <unistd.h>
+#include <map>
 
 class Server {
 private:
     int                     serversocket;
+    int                     epfd;
     int                     port;
     struct sockaddr_in      address;
+    struct epoll_event      ev;
     std::string             password;
+    std::map<int, std::string> client;
     // std::vector<pollfd>     fds;
 
 public:
@@ -20,9 +25,13 @@ public:
     const Server& operator=(const Server& other);
     ~Server();
 
-    void    initserver();
-    void    run();
-    void    acceptNewClient();
+    std::string get_password();
+    void        add_client(int fd);
+    bool        isNicknameTaken(const std::string& nickname) const;
+    void        initserver();
+    void        run();
+    void        multiplexar();
+    void        acceptNewClient();
 };
 
 #endif
